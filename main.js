75021b528a2592c273445b73bc193b1d2747bd45
Andy Lamothe-Futu <2272637@collegemv.qc.ca>
2024-02-02 12:33:57 -0500

    ajout

---
+++ main.js
@@ -0,0 +1,6 @@
+function recup(){
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+    localStorage.setItem(user)
+    alert("user")
+}
